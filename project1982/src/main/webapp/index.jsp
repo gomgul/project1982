13ae7836d3a357ddee635c69ba267b2d08be4190
gjqm1 <gjqm1@LAPTOP-GMVSODPT>
2021-12-13 15:54:36 +0900

    lee

--- project1982/src/main/webapp/index.jsp
+++ project1982/src/main/webapp/index.jsp
@@ -1,21 +1,19 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-	<title>Home</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>index.jsp</title>
 </head>
 <body>
-<h1>
-	Hello world!  
-</h1>
+<h1>환영합니다</h1>
 
-<p> <a href='user/userLogin.do'>로그인</a> </p>
-<p> <a href='user/userJoin.do'>회원가입</a></p>
+<hr/>
 
-<a href='user/sample.do'>ㅇㄹㅇㄹ</a>
+<a href="login/1_login.do">로그인</a>
+<a href="login/2_register.do">회원가입</a>
 
-
+<a href="">메인페이지</a>
 </body>
 </html>
-
--- project1982/src/main/webapp/resources/js/js_userinput.js
+++ project1982/src/main/webapp/resources/js/js_userinput.js
@@ -1,50 +1,94 @@
 $(function(){
 	// 사용자의 자료 입력여부를 검사하는 함수
 	$('#confirm').click(function(){
-    	if( $.trim($("#userId").val()) == '' ){
+    	if( $.trim($("#userid").val()) == '' ){
             alert("아이디를 입력해 주세요.");
             $("#userId").focus();
             return;
         }
     	
-    	if($.trim($('#userPass').val())==''){
+    	if($.trim($('#userpass').val())==''){
     		alert("비번입력해주세요.");
     		$('#userPass').focus();
     		return;
     	}
     	
-    	if($.trim($('#userPass').val()) != $.trim($('#userPass2').val())){
+    /*	if($.trim($('#userpass').val()) != $.trim($('#userPass2').val())){
     		alert("비밀번호가 일치하지 않습니다..");
     		$('#userPass2').focus();
     		return;
-    	}
+    	}*/
     	
+    	if($.trim($('#usernick').val())==''){
+    		alert("닉네임을 입력해주세요.");
+    		$('#usernick').foucs();
+    		return;
+    	}    	
     	
-    	if($.trim($('#userName').val())==''){
+    	if($.trim($('#username').val())==''){
     		alert("이름입력해주세요.");
     		$('#userName').foucs();
     		return;
     	}
+    	
+    	if($.trim($('#usercode').val())==''){
+    		alert("주민등록번호를 입력해주세요.");
+    		$('#usercode').foucs();
+    		return;
+    	}
        
+    	if($.trim($('#usergender').val())==''){
+    		alert("성별을 입력해주세요.");
+    		$('#usergender').foucs();
+    		return;
+    	}
+    	
+    	if($.trim($('#userpn').val())==''){
+    		alert("휴대전화번호를 입력해주세요.");
+    		$('#userpn').foucs();
+    		return;
+    	}
+    	
+    	if($.trim($('#useraddr').val())==''){
+    		alert("주소를 입력해주세요.");
+    		$('#useraddr').foucs();
+    		return;
+    	}
+    	
+    /*	if($.trim($('#usermail').val())==''){
+    		alert("메일을 입력해주세요.");
+    		$('#usermail').foucs();
+    		return;
+    	}*/
+    	
         // 자료를 전송합니다.
         document.userinput.submit();
 	});
 	
 	//아이디 중복체크
-	$('#userId').keyup(function(){
-        $.ajax({
-        type: 'get',
-        url: 'idCheck.do',
-        data:{userId : $('#userId').val()},
-        //한글 처리하는 부분
-        contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
-        success : function(result){
-        	$('#idCheckResult').text(result);
-        },
-        error : function(err){
-        	alert('fail');
-        }
-        })
-       
+	$('#userㅑd').keyup(function(){
+        
+		$.ajax({
+			type : 'get',
+			url : 'idCheck.do',
+			data : {userid : $('#userid').val()},
+			// 한글처리
+			contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+			success : function(result){
+				$('#idCheckResult').text(result);
+			},
+			error : function(err){
+				alert('fail');
+			}
+		});
 	})
 })
+	
+	
+	
+	
+	
+	
+	
+	
+	
